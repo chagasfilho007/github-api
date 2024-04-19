c9a04acff0c9dd7c4afc18f7ac750f91ffb57eeb
Chagas Filho <106563976+chagasfilho007@users.noreply.github.com>
2024-04-19 15:30:19 -0300

    finalizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,32 @@
-const respósitorios = document.querySelector('.conteudo-main')
+const respositories = document.querySelector('.conteudo-main');
 
 function getApiGithub(){
-    fetch('https://api.github.com/users/chagasfilho007?tab=repositories')
-    .then(async res => {
-        if( !res.ok){
-            throw new Error(res.status)
-        }
+    fetch('https://api.github.com/users/chagasfilho007/repos')
+        .then(async res => {
+            if( !res.ok) {
+                throw new Error(res.status);
+            }
 
-        let data = await res.json()
-        data.map(item => {
-            let project = document.createElement('div')
+            let data = await res.json();
+            data.map( item => {
+                let projeto = document.createElement('div');
 
-            project.innerHTML = `teste`
+                projeto.innerHTML = `
+                <div class="projeto">
+                    <div>
+                        <h4 class="titulo">${ item.name}</h4>
+                        <span class="data-criada">${ Intl.DateTimeFormat('pt-br').format(new Date(item.created_at)) }</span>
+                        </div>
+                        <div>
+                        <a href="${ item.html_url}" target="_blank">${ item.html_url}</a>
+                        <span class="linguagem"><samp class="circulo"></samp>${ item.language}</span>
+                    </div>
+                </div>
+                `
 
-            respositories.appendChild(project);
+                respositories.appendChild(projeto);
+            })
         })
-    })
 }
 
-getApiGithub();
+getApiGithub()
